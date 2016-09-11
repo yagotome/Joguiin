b39a7a5d0f14f782ac8289a11ee2aa0ba2209483
Yago Tomé <yago.tome@hotmail.com>
2016-09-10 23:49:05 -0300

    Finalizando primeira versão

--- main.lua
+++ main.lua
@@ -17,11 +17,14 @@ function love.load()
   player.travado = false
 
   bomba = Bomba.newBomba(player)
+  t = 0
+  acabou = false
 end
 
 function love.keypressed(key)
   if key == 'space' then
     player.travado = true
+    bomba.em_movimento = true
   end
 end
 
@@ -78,13 +81,56 @@ function love.update(dt)
     --     player.y = player.ground
     -- end
   end
+
+  -- if fisica.colide(bomba, player) == false and bomba.y > player.y + player.h then
+  --   bomba.em_movimento = false
+  --   t = 0
+  --   if player == player1 then
+  --     player = player2
+  --   else
+  --     player = player1
+  --   end 
+  --   player.travado = false    
+  --   bomba.atualizaPosicao(player)
+  -- end
+
+  if player == player1 and fisica.colide(bomba, player2) == false and bomba.y > player.y + player.h then
+    bomba.em_movimento = false
+    t = 0
+    player = player2
+    player.travado = false
+    bomba.atualizaPosicao(player)
+  elseif player == player2 and fisica.colide(bomba, player1) == false and bomba.y > player.y + player.h then
+    bomba.em_movimento = false
+    t = 0
+    player = player1
+    player.travado = false
+    bomba.atualizaPosicao(player)
+  end  
+
+  if bomba.em_movimento == true then
+    if player == player1 and fisica.colide(bomba, player2) or player == player2 and fisica.colide(bomba, player1) then
+      acabou = true
+    end
+    if (player == player1) then
+      bomba.x = fisica.mu_s(bomba.x, 100, dt)
+    else
+      bomba.x = fisica.mu_s(bomba.x, -100, dt)
+    end
+    bomba.y = fisica.muv_s(bomba.y, -10, t, 10)
+  end
+  t = t + dt
 end
 
 function love.draw()
-  love.graphics.draw(background)
-  love.graphics.draw(player1.img, player1.x, player1.y, 0, 0.34, 0.34)
-  love.graphics.draw(player2.img, player2.x, player2.y, 0, 0.34, 0.34)
-  -- love.graphics.draw(player1.mira.img, player1.mira.x, player1.mira.y, player1.mira.angulo, 0.34, 0.34)
-  -- love.graphics.draw(player2.mira.img, player2.mira.x, player2.mira.y, player2.mira.angulo, 0.34, 0.34)
-  love.graphics.draw(bomba.img, bomba.x, bomba.y, bomba.angulo, 0.12, 0.12)
+  if acabou then
+    love.graphics.print('Game over', love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+  else
+    love.graphics.draw(background)
+    love.graphics.draw(player1.img, player1.x, player1.y, 0, 0.34, 0.34)
+    love.graphics.draw(player2.img, player2.x, player2.y, 0, 0.34, 0.34)
+    -- love.graphics.draw(player1.mira.img, player1.mira.x, player1.mira.y, player1.mira.angulo, 0.34, 0.34)
+    -- love.graphics.draw(player2.mira.img, player2.mira.x, player2.mira.y, player2.mira.angulo, 0.34, 0.34)
+    love.graphics.draw(bomba.img, bomba.x, bomba.y, bomba.angulo, 0.12, 0.12)
+  end
 end
