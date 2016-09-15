1e161ee90d2c39df4afcd933567b3fcfb05ce0e3
Yago Tomé <yago.tome@hotmail.com>
2016-09-15 18:18:44 -0300

    Alteração no movimento da bomba
    Movimento está mais estável com a nova implementação que guarda a
    posicao inicial da bomba em y com o uso de closure.

--- fisica.lua
+++ fisica.lua
@@ -6,13 +6,16 @@ exports.mu_s = function (s0, v, t)
     return s0 + v*t
 end
 
-exports.muv_s = function (s0, v0, t, a) 
-    return s0 + v0*t + a*t*t/2
+exports.muv_s = function (s0)
+    local s_inicial = s0
+    return function (v0, t, a)
+        return s_inicial + v0*t + a*t*t/2        
+    end
 end
 
 exports.colide = function (o1, o2)
-     return (o1.x+o1.w >= o2.x) and (o1.x <= o2.x+o2.w) and
-           (o1.y+o1.h >= o2.y) and (o1.y <= o2.y+o2.h)
+    return (o1.x+o1.w >= o2.x) and (o1.x <= o2.x+o2.w) and
+            (o1.y+o1.h >= o2.y) and (o1.y <= o2.y+o2.h)
 end
 
 return exports
